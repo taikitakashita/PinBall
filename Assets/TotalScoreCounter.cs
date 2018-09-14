355282387ca4b5a6d58c6d60890b18a5b1643858
Taiki Takashita <taiki.takashita@qdenbs.com>
2018-09-14 16:00:38 +0900

    PinBallプロジェクト Lesson5「3Dゲームアプリを作ろう Vol.1」の課題提出
    Lesson5「3Dゲームアプリを作ろう Vol.1」の課題にて作成したファイルです。

---
+++ Assets/TotalScoreCounter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TotalScoreCounter : MonoBehaviour {
+
+
+    // 合計スコアの初期化
+    private int totalscore = 0;
+
+    //合計を表示するテキスト
+    private GameObject totalscoreText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //シーン中のTotalScoreオブジェクトを取得
+        this.totalscoreText = GameObject.Find("TotalScoreText");
+
+        //TotalScoreの初期表示
+        this.totalscoreText.GetComponent<Text>().text = "TotalScore：" + this.totalscore.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        // 合計スコアにターゲットごとに取得できるスコアを加点
+        if (other.gameObject.CompareTag("SmallStarTag"))
+        {
+            this.totalscore += 10;
+        }
+        else if (other.gameObject.CompareTag("LargeStarTag") || other.gameObject.CompareTag("SmallCloudTag"))
+        {
+            this.totalscore += 20;
+        }
+        else if (other.gameObject.CompareTag("LargeCloudTag"))
+        {
+            this.totalscore += 50;
+        }
+
+        // TotalScoreTextの表示を更新
+        this.totalscoreText.GetComponent<Text>().text = "TotalScore：" + this.totalscore.ToString();
+    }
+}
